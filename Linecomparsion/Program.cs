74477ea8d8f5e99e5c35b844a4a88ed887ac7ab0
imohtesham <ibraahimm92@gmail.com>
2022-08-23 16:59:10 +0530

    Length of line using OOPS(With Equals and Comparetomethod)

--- Linecomparsion/Program.cs
+++ Linecomparsion/Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace Linecomparsion
 {
-    public class Program
+    public class line
 
     {
-       static void Main(string[] args)
+        int x1;
+        int x2;
+        int y1;
+        int y2;
+
+        static void Main(string[] args)
         {
+            
             Console.WriteLine("Enter start line(x1, y1) and End-point(x2 , y2) co-ordinates of line : ");
 
             Console.Write("x1 : ");
@@ -19,9 +25,24 @@
             //length
             double length = Math.Sqrt(x2 - x1) * (y2 - y1);
             Console.WriteLine("Length of line have cooridnates(0,1) and (2,3) IS :(4)", x1, x2, y1, y2, length);
+            
+
+            line myobj = new line();
+    
+            myobj.x1 = x1; myobj.y1 = y1;
+            myobj.x2 = x2; myobj.y2 = y2;
+
+            Console.WriteLine(myobj.x1);
+            Console.WriteLine(myobj.y1);
+            Console.WriteLine(myobj.x2);
+            Console.WriteLine(myobj.y2);
 
 
-            //CompareTo
+            Console.WriteLine("Using equals() method: {0}", x1.Equals(y1));
+            Console.WriteLine("Using equals() method: {0}", x2.Equals(y2));
+
+
+            //CompareTso
             int status = x1.CompareTo(y1);
 
             // checking the status
@@ -33,6 +54,20 @@
 
             else
                 Console.WriteLine("{0} is equal to {1}", x1, y1);
+            Console.ReadLine();
+
+
+            int status2 = x2.CompareTo(y2);
+
+            // checking the status
+            if (status > 0)
+                Console.WriteLine("{0} is greater than {1}", x2, y2);
+
+            else if (status < 0)
+                Console.WriteLine("{0} is less than {1}", x2, y2);
+
+            else
+                Console.WriteLine("{0} is equal to {1}", x2, y2);
             Console.ReadLine();
 
 
